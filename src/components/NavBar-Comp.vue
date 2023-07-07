a4420374ba4761c0cebdaf8b06bc1a87ef38c643
MarcelBrown17 <130047013+MarcelBrown17@users.noreply.github.com>
2023-07-07 15:29:25 +0200

    Update NavBar-Comp.vue

--- src/components/NavBar-Comp.vue
+++ src/components/NavBar-Comp.vue
@@ -1,6 +1,6 @@
 <template>
      <header>
-    <div class="nav-container pb-5">
+    <div class="nav-container pb-4">
       <nav class="navbar navbar-dark bg-dark fixed-top">
   <div class="container-fluid">
     <h1 class="navbar-brand">Marcel Brown</h1>
